a93796cc571b6a8a1349938035f277947fe489b5
Pedro Henrique Vengrus <phvengrus@gmail.com>
2025-05-26 01:05:01 -0300

    ex3 e ajustes ex2

--- Complexidade de Algoritmos/ex2.py
+++ Complexidade de Algoritmos/ex2.py
@@ -1,12 +1,12 @@
 import random
 
-vetor1 = [random.randint(1, 100) for i in range (20)]
+vetor1 = [random.randint(1, 100) for i in range(20)]
 print(vetor1)
 
-vetor2 = [random.randint(1, 100) for i in range (20)]
+vetor2 = [random.randint(1, 100) for i in range(20)]
 print(vetor2)
 
-vetor3 = [random.randint(1, 100) for i in range (20)]
+vetor3 = [random.randint(1, 100) for i in range(20)]
 print(vetor3)
 
 print(" ")
@@ -30,4 +30,17 @@ if valor in vetor3:
     print(f"O valor {valor} foi encontrado no terceiro vetor e está na {posicao + 1}° posição")
 else:
     print(f"O valor {valor} não foi encontrado no terceiro vetor")
-    
+
+def buscaLinear_nao_ordenado(vetor, valor):
+
+    for i in range(len(vetor)): # Percorre o vetor até encontrar o valor
+        if vetor[i] == valor:
+            return i  # Retorna a posicao do valor
+    return None  # Retorna None se nao encontrar
+
+print(" ")
+
+print("Saida =", buscaLinear_nao_ordenado(vetor1, valor) + 1)
+print("Saida =", buscaLinear_nao_ordenado(vetor2, valor) + 1)
+print("Saida =", buscaLinear_nao_ordenado(vetor3, valor) + 1)
+
---
+++ Complexidade de Algoritmos/ex3.py
@@ -0,0 +1,61 @@
+import random
+
+numeros_sorteados = random.sample(range(1, 61), 6) 
+print(numeros_sorteados, end="\n\n")
+
+numeros_escolhidos = []
+
+for i in range(6):
+    valor = int(input(f"Insira o {i + 1}° numero: "))
+    while valor in numeros_escolhidos:
+        print("Nao pode repetir o mesmo numero!")
+        valor = int(input(f"Insira o {i + 1}° numero: "))
+
+    numeros_escolhidos.append(valor)
+
+print(f"\nOs numeros escolhidos foram", end=" ")
+
+for n in numeros_escolhidos[:-1]:
+    print(n, end=", ")
+
+print(numeros_escolhidos[-1])
+
+def buscaLinear_nao_ordenado(vetor, valor):
+
+    for i in range(len(vetor)): # Percorre o vetor até encontrar o valor
+        if vetor[i] == valor:
+            return i  # Retorna a posicao do valor
+    return None  # Retorna None se nao encontrar
+
+acertos_numeros = []
+
+for n in numeros_escolhidos:
+    if buscaLinear_nao_ordenado(numeros_sorteados, n) != None:
+        acertos_numeros.append(n)
+
+print("Os numeros que voce acertou foram", end=" ")
+
+for n in acertos_numeros[:-1]:
+    print(n, end=", ")
+
+if acertos_numeros:
+    for n in acertos_numeros[:-1]:
+        print(n, end=", ")
+    print(acertos_numeros[-1])
+else:
+    print("0")
+
+print(f"Voce acertou o total de {len(acertos_numeros)} numeros")
+
+if len(acertos_numeros) == 6:
+    print("Voce ganhou!")
+else:
+    print("Voce perdeu!")
+
+
+
+        
+
+
+
+
