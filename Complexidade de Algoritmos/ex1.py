c994886a60e3f2eb0ad4addc075d47f32e01a280
Pedro Henrique Vengrus <phvengrus@gmail.com>
2025-05-26 01:19:28 -0300

    correcao ex1

--- Complexidade de Algoritmos/ex1.py
+++ Complexidade de Algoritmos/ex1.py
@@ -2,13 +2,17 @@ busca_linear = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 valor = int(input("Insira um valor para buscar: "))
 
+print(" ")
+
 if valor in busca_linear:
     print(f"O valor {valor} foi encontrado na lista")
+    posicao = busca_linear.index(valor) + 1
+    print(f"Ele esta na {posicao}° posição")
 else:
     print(f"Não existe o valor {valor} na lista")
 
-posicao = busca_linear.index(valor) + 1
-print(f"Ele esta na {posicao}° posição")
+
+
 
 
 
